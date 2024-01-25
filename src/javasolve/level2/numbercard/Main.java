260dabad9b0321f0b79207f852efd231fd0e6899
jongchan <jongchan1009@naver.com>
2024-01-25 20:10:58 +0900

    2024-01-22 [level2][Java]

--- src/javasolve/level2/numbercard/Main.java
+++ src/javasolve/level2/numbercard/Main.java
@@ -1,34 +1,28 @@
+package javasolve.level2.numbercard;
+
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
 class Solution {
-	public int solution(int[] arrayA, int[] arrayB) {
+    public int solution(int[] arrayA, int[] arrayB) {
 
-		int gcdA = totalGcd(arrayA);
-		int gcdB = totalGcd(arrayB);
+        int gcdA = totalGcd(arrayA);
+        int gcdB = totalGcd(arrayB);
 
-		List<Integer> listA = divisor(gcdA);
-		List<Integer> listB = divisor(gcdB);
+        List<Integer> listA = divisor(gcdA);
+        List<Integer> listB = divisor(gcdB);
 
-		int resultA = 0;
-		int resultB = 0;
+        int resultA = 0;
+        int resultB = 0;
 
-        for(int divisor : listA) {
-            boolean flag = true;
-            for(int numB : arrayB) {
-                if(numB % divisor == 0) {
-                    flag = false;
-                    break;
-                }
-            }
-                
-            if(flag) {
-                resultA = divisor;
-                break;
-            }
-        }
-        
+        resultA = getResult(arrayB, listA, resultA);
+
+        resultB = getResult(arrayA, listB, resultB);
+        return Math.max(resultA,resultB);
+    }
+
+    private int getResult(int[] arrayA, List<Integer> listB, int resultB) {
         for(int divisor : listB) {
             boolean flag = true;
             for(int numA : arrayA) {
@@ -37,48 +31,58 @@ class Solution {
                     break;
                 }
             }
-                
+
             if(flag) {
                 resultB = divisor;
                 break;
             }
         }
-		return Math.max(resultA,resultB);
-	}
+        return resultB;
+    }
 
-	public int gcd(int n, int r) {
-		int m = n % r;
-		if(m == 0) {
-			return r;
-		}
-		return gcd(r, m);
-	}
+    public int gcd(int n, int r) {
+        int m = n % r;
+        if(m == 0) {
+            return r;
+        }
+        return gcd(r, m);
+    }
 
-	public int totalGcd(int[] array) {
+    public int totalGcd(int[] array) {
 
-		int total = array[0];
+        int total = array[0];
 
-		for(int num : array) {
-			total = gcd(total, num);
-		}
-		return total;
-	}
+        for(int num : array) {
+            total = gcd(total, num);
+        }
+        return total;
+    }
 
-	public List<Integer> divisor(int n) {
-		int sqrt = (int)Math.sqrt(n);
+    public List<Integer> divisor(int n) {
+        int sqrt = (int)Math.sqrt(n);
 
-		List<Integer> list = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
 
-		for(int i = 1 ; i<= sqrt ; i++) {
-			if(n % i == 0 ){
-				list.add(i);
-				if(i != n / i) {
-					list.add(n / i);
-				}
-			}
-		}
-		list.sort(Comparator.reverseOrder());
+        for(int i = 1 ; i<= sqrt ; i++) {
+            if(n % i == 0 ){
+                list.add(i);
+                if(i != n / i) {
+                    list.add(n / i);
+                }
+            }
+        }
+        list.sort(Comparator.reverseOrder());
 
-		return list;
-	}
+        return list;
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] arrayA = new int[]{10, 20};
+        int[] arrayB = new int[]{5, 17};
+        int result = (new Solution()).solution(arrayA, arrayB);
+        System.out.println("result = " + result);
+    }
 }
