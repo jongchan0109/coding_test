808661fe2be340c2fd25686eda2a70419a39d9dc
jongchan <jongchan1009@naver.com>
2024-02-04 17:38:54 +0900

    2024-02-04 [level2][Java]

---
+++ src/javasolve/level2/nsquarearray/Main.java
@@ -0,0 +1,30 @@
+package javasolve.level2.nsquarearray;
+
+class Solution {
+    public int[] solution(int n, long left, long right) {
+        int len = (int)(right - left) + 1;
+        int[] answer = new int[len];
+        int index = 0;
+
+        for (long i = left ; i <= right; i++, index++) {
+            int row = (int) (i / n);
+            int col = (int) (i % n);
+            answer[index] = Math.max(row,col) + 1;
+        }
+        return answer;
+
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        int n = 3;
+        long left = 2L;
+        long right = 5L;
+        int[] result = new Solution().solution(n,left,right);
+
+        for (int i : result) {
+            System.out.print(i+" ");
+        }
+    }
+}
