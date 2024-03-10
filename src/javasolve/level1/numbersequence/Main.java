96b40b8271824d7a2a77ff80521c369071f498e2
jongchan <jongchan1009@naver.com>
2024-03-10 17:22:12 +0900

    2024-03-10 [level1][Java]

---
+++ src/javasolve/level1/numbersequence/Main.java
@@ -0,0 +1,29 @@
+package javasolve.level1.numbersequence;
+
+class Solution {
+    public int solution(String t, String p) {
+        int answer = 0;
+        int len = p.length();
+        long pNum = Long.parseLong(p);
+
+
+
+        for (int i = 0; i <= t.length() - len; i++) {
+            String sub = t.substring(i,i+len);
+            if (Long.parseLong(sub) <= pNum) {
+                answer++;
+            }
+
+        }
+        return answer;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        String t = "3141592";
+        String p = "271";
+        int result = new Solution().solution(t,p);
+        System.out.println("result = " + result);
+    }
+}
