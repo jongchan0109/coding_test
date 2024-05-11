8955e268b28307457f79baf97fac744e20155381
jongchan <jongchan1009@naver.com>
2024-05-11 14:34:05 +0900

    2024-05-10 [D4][Java]

--- src/swea/d4/d4_1210/Solution.java
+++ src/swea/d4/d4_1210/Solution.java
@@ -2,9 +2,8 @@ package swea.d4.d4_1210;
 
 import java.util.Scanner;
 
-class Solution {
-
-    static int[] dx = {0, 0, -1};
+public class Solution {
+    static int[] dx = {0, 0, 1};
     static int[] dy = {1, -1, 0};
     static boolean[][] visited;
     static int answer;
@@ -12,7 +11,6 @@ class Solution {
     public static void main(String[] args) throws Exception {
 
         Scanner sc = new Scanner(System.in);
-
 
         for (int test_case = 1; test_case <= 10; test_case++) {
             int T;
@@ -25,45 +23,37 @@ class Solution {
                 }
             }
 
-            int result = 0;
-            for (int i =0; i<100; i++) {
+            for (int i = 0; i < 100; i++) {
                 if (map[0][i] == 1) {
                     visited = new boolean[100][100];
-                    visited[0][i] = true;
-                    dfs(map,0, i);
-
-                    if (answer != -1) {
-                        result = i;
+                    dfs(map, 0, i);
+                    if (answer == 2) {
+                        System.out.println("#" + T + " " + i);
                         break;
                     }
                 }
             }
-            System.out.println("#" + T + " " + result);
-
         }
     }
 
-    public static void dfs(int[][] map,int a, int b) {
+    public static void dfs(int[][] map, int row, int col) {
 
-        if (a == 99) {
-            if (map[a][b] == 2) {
-                answer = 2;
-            }
+        if (row == 99) {
+            answer = map[row][col];
         }
 
-        for (int i=0; i<3; i++) {
+        for (int i = 0; i < 3; i++) {
+            int x = row + dx[i];
+            int y = col + dy[i];
 
-            int x = a + dx[i];
-            int y = b + dy[i];
-
-            if (x>= 0 && y>=0 && x<100 && y<100) {
-                if (!visited[x][y] && map[x][y] == 1 || map[x][y] == 2) {
-                    visited[x][y] = true;
-                    dfs(map, x,y);
-                    break;
-                }
+            if (0 <= x && x <= 99 && 0 <= y && y <= 99 && (map[x][y] == 1 || map[x][y] == 2) && !visited[x][y]) {
+                visited[x][y] = true;
+                dfs(map, x, y);
+                break;
             }
+
         }
+
     }
 
 }
