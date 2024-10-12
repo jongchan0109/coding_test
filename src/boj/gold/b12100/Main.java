037651e7955e18d82da938a97f8050f35fec4b43
jongchan0109 <jongchan1009@naver.com>
2024-10-12 14:34:19 +0900

    2024-10-12 [boj gold][Java]

--- src/boj/gold/b12100/Main.java
+++ src/boj/gold/b12100/Main.java
@@ -1,242 +1,154 @@
 package boj.gold.b12100;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    static int n;
+    /**
+     * start 13:38 1. 중복 순열을 이용하여 이동시키는 경로를 구한다. 2. 경로를 구한 후 해당 경로에 맞게 보드를 이동시킨다. 중복 순열 구현: DFS 보드
+     * 이동: 보드를 아래로 이동 시키는 한가지 메서드만 구현하고, 나머지는 회전시켜서 구한다. 필요 변수 int[][] map, int n, int[] perm, int
+     * max 필요 메서드: void dfs(int depth), void move(), void rotate(), int[][] cpy;
+     */
 
-    static int[][] startBoard;
+    //0 상, 1 우, 2 하, 3 좌
 
-    static int max;
+    private static int[][] map;
+    private static int n;
+    private static int[] perm;
+    private static int max;
 
-    public static void main(String[] args) {
-        input();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        n = Integer.parseInt(br.readLine());
+        perm = new int[5];
         max = 0;
-        dfs(0, startBoard);
+
+        map = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        dfs(0);
         System.out.println(max);
     }
 
-    private static void input() {
-        Scanner scanner = new Scanner(System.in);
-        n = scanner.nextInt();
-        startBoard = new int[n][n];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                startBoard[i][j] = scanner.nextInt();
-            }
-        }
-    }
-
-
-    private static void dfs(int depth, int[][] board) {
+    private static void dfs(int depth) {
         if (depth == 5) {
-            max = Math.max(max, getMax(board));
+            int[][] newMap = cpy();
+
+            for (int i = 0; i < 5; i++) {
+                newMap = switch (perm[i]) {
+                    case 0 -> rotate(newMap, 2);
+                    case 1 -> rotate(newMap, 1);
+                    case 3 -> rotate(newMap, 3);
+                    default -> newMap;
+                };
+
+                move(newMap);
+
+                newMap = switch (perm[i]) {
+                    case 0 -> rotate(newMap, 2);
+                    case 1 -> rotate(newMap, 3);
+                    case 3 -> rotate(newMap, 1);
+                    default -> newMap;
+                };
+            }
+            max = Math.max(max, getMax(newMap));
             return;
         }
 
-        int[][] left = left(board, true);
-
-        dfs(depth + 1, left);
-
-        int[][] right = right(board, true);
-
-        dfs(depth + 1, right);
-
-
-        int[][] up = up(board, true);
-
-        dfs(depth + 1, up);
-
-
-        int[][] down = down(board, true);
-
-        dfs(depth + 1, down);
+        for (int i = 0; i < 4; i++) {
+            perm[depth] = i;
+            dfs(depth + 1);
+        }
     }
 
-    private static int getMax(int[][] board) {
-        int max = Integer.MIN_VALUE;
+    private static int[][] cpy() {
+
+        int[][] newMap = new int[n][n];
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                max = Math.max(max, board[i][j]);
-            }
+            System.arraycopy(map[i], 0, newMap[i], 0, n);
         }
-        return max;
+        return newMap;
     }
 
-    static int[][] left(int[][] board, boolean flag) {
-        int[][] clone = deepCopy(board);
+    private static int[][] rotate(int[][] newMap, int direction) {
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 1; j < n; j++) {
+        for (int d = 0; d < direction; d++) {
+            int[][] rotateMap = new int[n][n];
 
-                int index = j;
-
-                while (index >= 1 && clone[i][index - 1] == 0) {
-                    clone[i][index - 1] = clone[i][index];
-                    clone[i][index] = 0;
-                    index--;
-                }
-            }
-        }
-        if (flag) {
-            return colMerge(clone, false);
-        }
-        return clone;
-    }
-
-    static int[][] right(int[][] board, boolean flag) {
-        int[][] clone = deepCopy(board);
-        for (int i = 0; i < n; i++) {
-            for (int j = n - 2; j >= 0; j--) {
-
-                int index = j;
-
-                while (index < n - 1 && clone[i][index + 1] == 0) {
-                    clone[i][index + 1] = clone[i][index];
-                    clone[i][index] = 0;
-                    index++;
-                }
-            }
-
-        }
-        if (flag) {
-            return colMerge(clone, true);
-        }
-        return clone;
-    }
-
-    static int[][] up(int[][] board, boolean flag) {
-        int[][] clone = deepCopy(board);
-
-        for (int j = 0; j < n; j++) {
-            for (int i = 1; i < n; i++) {
-                int index = i;
-
-                while (index >= 1 && clone[index - 1][j] == 0) {
-                    clone[index - 1][j] = clone[index][j];
-                    clone[index][j] = 0;
-                    index--;
-                }
-            }
-        }
-        if (flag) {
-            return rowMerge(clone, false);
-        }
-        return clone;
-    }
-
-    static int[][] down(int[][] board, boolean flag) {
-        int[][] clone = deepCopy(board);
-
-        for (int j = 0; j < n; j++) {
-            for (int i = n - 2; i >= 0; i--) {
-
-                int index = i;
-
-                while (index < n - 1 && clone[index + 1][j] == 0) {
-                    clone[index + 1][j] = clone[index][j];
-                    clone[index][j] = 0;
-                    index++;
-                }
-            }
-        }
-        if (flag) {
-            return rowMerge(clone, true);
-        }
-        return clone;
-    }
-
-
-    static int[][] colMerge(int[][] clone, boolean flag) {
-
-        int[][] board = deepCopy(clone);
-
-        if (!flag) {
             for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n - 1; j++) {
-
-                    if (board[i][j] == 0) {
-                        continue;
-                    }
-
-                    if (board[i][j] == board[i][j + 1]) {
-                        board[i][j] = 2 * board[i][j];
-                        board[i][j + 1] = 0;
-                        j++;
-                    }
+                for (int j = 0; j < n; j++) {
+                    rotateMap[j][n - 1 - i] = newMap[i][j];
                 }
             }
-        } else {
-            for (int i = 0; i < n; i++) {
-                for (int j = n - 1; j > 0; j--) {
-                    if (board[i][j] == 0) {
-                        continue;
-                    }
-                    if (board[i][j] == board[i][j - 1]) {
-                        board[i][j - 1] = 2 * board[i][j];
-                        board[i][j] = 0;
-                        j--;
-                    }
-                }
-            }
+            newMap = rotateMap;
         }
-
-        if (!flag) {
-            board = left(board, false);
-        } else {
-            board = right(board, false);
-        }
-        return board;
+        return newMap;
     }
 
-    static int[][] rowMerge(int[][] clone, boolean flag) {
+    private static void move(int[][] newMap) {
 
-
-        int[][] board = deepCopy(clone);
-
-        if (!flag) {
-            for (int j = 0; j < n; j++) {
-                for (int i = 0; i < n - 1; i++) {
-                    if (board[i][j] == 0) {
-                        continue;
-                    }
-                    if (board[i][j] == board[i + 1][j]) {
-                        board[i][j] = 2 * board[i][j];
-                        board[i + 1][j] = 0;
-                        i++;
-                    }
-                }
-            }
-        } else {
-            for (int j = 0; j < n; j++) {
-                for (int i = n - 1; i > 0; i--) {
-                    if (board[i][j] == 0) {
-                        continue;
-                    }
-                    if (board[i - 1][j] == board[i][j]) {
-                        board[i - 1][j] = 2 * board[i][j];
-                        board[i][j] = 0;
-                        i--;
-                    }
-                }
-            }
+        for (int col = 0; col < n; col++) {
+            moveCol(newMap, col);
+            mergeCol(newMap, col);
+            moveCol(newMap, col);
         }
 
-        if (!flag) {
-            board = up(board, false);
-        } else {
-            board = down(board, false);
-        }
-
-        return board;
     }
 
-    static int[][] deepCopy(int[][] original) {
-        int[][] result = new int[original.length][];
-        for (int i = 0; i < original.length; i++) {
-            result[i] = original[i].clone();
+    private static void moveCol(int[][] newMap, int col) {
+        int row = n - 1;
+        while (row >= 0) {
+            if (newMap[row][col] != 0) {
+                row--;
+                continue;
+            }
+            int nextR = row - 1;
+
+            while (nextR >= 0 && newMap[nextR][col] == 0) {
+                nextR--;
+            }
+            if (nextR == -1) {
+                break;
+            }
+            newMap[row][col] = newMap[nextR][col];
+            newMap[nextR][col] = 0;
+            row--;
         }
-        return result;
+    }
+
+    private static void mergeCol(int[][] newMap, int col) {
+
+        for (int row = n - 1; row > 0; row--) {
+            if (newMap[row][col] == 0) {
+                break;
+            }
+            if (newMap[row][col] == newMap[row - 1][col]) {
+                newMap[row][col] += newMap[row - 1][col];
+                newMap[row - 1][col] = 0;
+                row--;
+            }
+        }
+
+    }
+
+    private static int getMax(int[][] map) {
+
+        int m = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                m = Math.max(map[i][j], m);
+            }
+        }
+        return m;
     }
 }
